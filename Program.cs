8729993504ebc153c9e6ee94c6f6cd00f9bd4787
GildembergSO <gildembergoli@gmail.com>
2024-10-21 19:28:36 -0300

    Desafio concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia ("81991919292", "Tijolinho", "14321", 32);
+Console.WriteLine ($"Smartphone Nokia");
+nokia.Ligar ();
+nokia.InstalarAplicativo ("Instagram");
+
+Console.WriteLine ("___________________________________________");
+
+Smartphone iphone = new Iphone ("83996789890", "S6 Pro MAX", "12341", 64);
+Console.WriteLine ($"Smartphone iPhone");
+iphone.Ligar ();
+iphone.InstalarAplicativo ("Whatsapp");
